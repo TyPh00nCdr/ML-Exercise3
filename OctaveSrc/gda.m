957b45aa17d96781e68d073a60c3904abc4d5278
Jan Niklas Hennings <3henning@informatik.uni-hamburg.de>
2019-05-20 16:56:55 +0200

    Working implementation of inverse and determinat of sigma

--- OctaveSrc/gda.m
+++ OctaveSrc/gda.m
@@ -6,43 +6,50 @@ function gda
   feats_pos = features(positives); 
   feats_neg = features(negatives);
   
-  uPos = mean(feats_pos);
-  uNeg = mean(feats_neg);
+  uPos = mean(feats_pos)';
+  uNeg = mean(feats_neg)';
  
-  c = 0;
+  global covSigma = 0;
   for feat = feats_pos';
-    c += (feat - uPos) * (feat - uPos)';
+    covSigma += (feat - uPos) * (feat - uPos)';
   end
   for feat = feats_neg';
-    c += (feat - uNeg) * (feat - uNeg)';
+    covSigma += (feat - uNeg) * (feat - uNeg)';
   end
-  c /= (rows(feats_pos) + rows(feats_neg));
+  covSigma /= (rows(feats_pos) + rows(feats_neg));
+  global invCovSigma = inv(covSigma);
+  global detCovSigma = det(covSigma);
   
-  # cov = arrayfun(@(feat) inspect(feat), feats(:), "UniformOutput", false);
+  disp(detCovSigma);
   
 endfunction
 
-function i = inspect (x)
-  j = x;
-  i = j;
+function prob = probgiveny (x, y)
+  leftTerm = 1 / ((2 * pi)^(length(feat) / 2) * sqrt(detCovSigma));
+  if (y == 0)
+    prob = leftTerm * exp(-0.5 * (x - uNeg)' * invCovSigma * (x - uNeg));
+  elseif (y == 1) 
+    prob = leftTerm * exp(-0.5 * (x - uPos)' * invCovSigma * (x - uPos));
+  endif
 endfunction
 
 function img = imreadtodouble (im)
-  # Load a single image from path (im) as a matrix of doubles  
+  ## Load a single image from path (im) as a matrix of doubles  
   img = double(imread(im));
 endfunction
 
 function feats = features (doubleimgs)
-  # Collect all features from the cell array of (24 x 24 x 3) image matrices
-  feats = cell2mat(cellfun("rgbgrayfeats", doubleimgs, "UniformOutput", false));
+  ## Collect all features from the cell array of (24 x 24 x 3) image matrices
+  feats = cell2mat(cellfun("rgbfeats", doubleimgs, "UniformOutput", false));
   
-  function allfeats = rgbgrayfeats (doubleimg)
-    # Collect all features from a single (24 x 24 x 3) image matrix
-    allfeats = [feature(doubleimg(:, :, 1)) feature(doubleimg(:, :, 2)) feature(doubleimg(:, :, 3)) feature(rgb2gray(doubleimg))];
+  function allfeats = rgbfeats (doubleimg)
+    ## Collect all features from a single (24 x 24 x 3) image matrix
+    allfeats = [feature(doubleimg(:, :, 1)) feature(doubleimg(:, :, 2)) feature(doubleimg(:, :, 3))] #feature(rgb2gray(doubleimg))];
+    # allfeats = feature(rgb2gray(doubleimg));
   endfunction
 endfunction
 
 function feat = feature (doubleimg)
-  # Collect all features from a single (24 x 24) image matrix
+  ## Collect all features from a single (24 x 24) image matrix
   feat = [max(doubleimg(:)), min(doubleimg(:)), mean(doubleimg(:)), std(doubleimg(:)), var(doubleimg(:))];
 endfunction
